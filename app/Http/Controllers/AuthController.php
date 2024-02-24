27ac6f4ce150a66ad50144ac04f6956edeabd164
Micah Villaruz <micahmvillaruz@gmail.com>
2024-02-25 01:30:50 +0800

    Change username type to string

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'username' => 'required|alpha:ascii|max:255',
+            'username' => 'required|string|max:255',
             'password' => 'required'
         ]);
 
