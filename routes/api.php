edaf9e345292b7c8a18d2dc5ab008a72b301651d
Micah Villaruz <micahmvillaruz@gmail.com>
2024-02-24 21:37:49 +0800

    Add login, logout and protect routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,18 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('/artists', ArtistController::class);
-Route::apiResource('/albums', AlbumController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::controller(DashboardController::class)->group(function () {
-    Route::prefix('dashboard')->group(function () {
+    Route::apiResource('/artists', ArtistController::class);
+    Route::apiResource('/albums', AlbumController::class);
+
+    Route::controller(DashboardController::class)->prefix('dashboard')->group(function () {
         Route::get('total-sales', 'totalAlbumsSoldPerArtist');
         Route::get('sales-per-artist', 'combinedAlbumSalesPerArtist');
         Route::get('top-artist', 'topSellingArtist');
